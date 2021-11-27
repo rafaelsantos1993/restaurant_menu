eac9dd9e6220ca2eb02dc3ea737dc93af2b385ae
rafaelsantos1993 <rafaellemos123@gmail.com>
2021-11-27 13:38:21 -0300

    Some grammatical fixes

--- database_setup.py
+++ database_setup.py
@@ -4,22 +4,24 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.sql.expression import false
+
 Base=declarative_base()
 
-class Resturant(Base):
+class Restaurant(Base):
     __tablename__='restaurant'
     
     id = Column(Integer, primary_key=True)
-    name=Column(String(250,nullable=False))
+    name=Column(String(250),nullable=False)
 
 class MenuItem(Base):
-    __table__name='menu_item'
+    __tablename__='menu_item'
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
     course=Column(String(250))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id')) 
     restaurant = relationship(Restaurant)
 
 #Serialize function to send Json objets in a serializable format
@@ -34,8 +36,8 @@ def serialize(self):
         'course'        :self.course,   
     }
 
-
+ 
 engine=create_engine('sqlite:///restaurantmenu.db')
 
-base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
